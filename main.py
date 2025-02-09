8931ad7a0dc4d37352e5fb08a0495057442eaa01
Anonymouscn <pgl888999@icloud.com>
2025-02-10 01:18:19 +0800

    feature: 优化模型交互逻辑体验

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ Send /start to initiate the conversation.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
-import datetime
+
 import json
 import logging
 import os
-import time
 import requests
 from md2tgmd import escape
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
@@ -58,7 +57,7 @@ logger = logging.getLogger(__name__)
 
 # GENDER, PHOTO, LOCATION, BIO = range(4)
 
-START, CHECK_HISTORY, CONTINUE_LAST, SELECT_HISTORY, NEW_CHAT, CHAT, SEND_PROMPT_TEXT = range(7)
+START, CHECK_HISTORY, CONTINUE_LAST, SELECT_HISTORY, NEW_CHAT, CREATE_PROMPT, SEND_PROMPT_TEXT = range(7)
 
 
 # async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -145,49 +144,75 @@ START, CHECK_HISTORY, CONTINUE_LAST, SELECT_HISTORY, NEW_CHAT, CHAT, SEND_PROMPT
 #     logger.info("Bio of %s: %s", user.first_name, update.message.text)
 #     await update.message.reply_text("Thank you! I hope we can talk again some day.")
 #
-#     return ConversationHandler.END
+#     return ConversationHandler.EN
 
 
 # 取消操作
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Cancels and ends the conversation."""
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
+    logger.info("User [%s] canceled the conversation.", user.full_name)
     await update.message.reply_text(
         "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
     )
     return ConversationHandler.END
 
 
+# readme 说明/获取帮助
 async def readme(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Cancels and ends the conversation."""
+    """Get help for """
     user = update.message.from_user
-    logger.info("User %s ask for help", user.first_name)
+    logger.info("User [%s] ask for help", user.full_name)
     await update.message.reply_text(
         "Anonymous X Bot is coming soon!\n\n"
         "/help 查看帮助\n"
         "/gpt 使用 chatgpt (已支持)\n"
-        "/deepseek 使用 deepseek (正在支持)\n"
-        , reply_markup=ReplyKeyboardRemove()
+        "/deepseek 使用 deepseek (正在支持)\n\n",
+        reply_markup=ReplyKeyboardMarkup(
+            [['/gpt', '/deepseek']],
+            resize_keyboard=True,
+            is_persistent=False,
+            one_time_keyboard=True,
+            input_field_placeholder="start chat"
+        ),
     )
     return ConversationHandler.END
 
 
 # gpt 开始切点
-async def gpt_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and asks the user about their gender."""
+async def chatgpt_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    return await chat_start(update, context, 'ChatGPT')
+
+
+# deepseek 开始切点
+async def deepseek_start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    return await chat_start(update, context, 'DeepSeek')
+
+
+# 开始切点
+async def chat_start(update: Update, context: ContextTypes.DEFAULT_TYPE, factory: str) -> int:
     user = update.message.from_user
-    options = ['new chat', '/cancel']
+    options = ['/new_chat', '/cancel']
     if user.name in chat_cache and len(chat_cache[user.name]) > 0:
-        options.append('continue')
-        options.append('history')
+        options.append('/continue')  # 继续上一次聊天
+        options.append('/history')  # 选择上次聊天历史
     else:
         chat_cache[user.name] = {}
+    tips = ("Hi! Welcome to use " +
+            factory +
+            " in Anonymous X !\n" +
+            "Send /cancel to stop talking to me.\n\n"
+            )
+    for option in options:
+        tips = tips + (option + "\n")
     await update.message.reply_text(
-        "Hi! Welcome to use OpenAI in Anonymous X !"
-        "Send /cancel to stop talking to me.\n\n",
+        tips,
         reply_markup=ReplyKeyboardMarkup(
-            [options], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="start chat"
+            [options],
+            resize_keyboard=True,
+            is_persistent=True,
+            one_time_keyboard=True,
+            input_field_placeholder="start chat"
         ),
     )
     return CHECK_HISTORY
@@ -198,29 +223,37 @@ async def check_history(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     selected = update.message.text
     user = update.message.from_user
     match selected:
-        case "continue":
+        case "/continue":
             await update.message.reply_text(
                 "Hi! Good to see U again !"
                 "Send /cancel to stop talking to me.\n\n",
                 reply_markup=ReplyKeyboardMarkup(
-                    [['/cancel']], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="continue chat"
+                    [['/cancel']],
+                    resize_keyboard=True,
+                    is_persistent=True,
+                    one_time_keyboard=True,
+                    input_field_placeholder="continue last chat"
                 ),
             )
             return SEND_PROMPT_TEXT
-        case "history":
+        case "/history":
             chat_names = []
             for chat_name in chat_cache[user.name]:
                 chat_names.append(chat_name)
             chat_names.append('/cancel')
             await update.message.reply_text(
-                "Hi! Welcome to use OpenAI in Anonymous X !"
+                "Hi! Select a chat in history to continue !\n"
                 "Send /cancel to stop talking to me.\n\n",
                 reply_markup=ReplyKeyboardMarkup(
-                    [chat_names], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="select chat"
+                    [chat_names],
+                    resize_keyboard=True,
+                    is_persistent=True,
+                    one_time_keyboard=True,
+                    input_field_placeholder="select chat"
                 ),
             )
             return SELECT_HISTORY
-        case "new chat":
+        case "/new_chat":
             user = update.message.from_user
             context_cache[user.name] = []
             if user.name not in chat_cache:
@@ -228,14 +261,18 @@ async def check_history(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
                     "": {"": []}
                 }
             await update.message.reply_text(
-                "Create a chat name to chat with Anonymous X !"
+                "Create a chat name to chat with Anonymous X !\n"
                 "Send /cancel to stop talking to me.\n\n",
                 reply_markup=ReplyKeyboardMarkup(
-                    [['/cancel']], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="chat name"
+                    [['/cancel']],
+                    resize_keyboard=True,
+                    is_persistent=True,
+                    one_time_keyboard=True,
+                    input_field_placeholder="chat name"
                 ),
             )
             return NEW_CHAT
-    return CHAT
+    return CREATE_PROMPT
 
 
 # gpt 新建聊天
@@ -253,22 +290,14 @@ async def new_chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         "Select a model to chat in Anonymous X !"
         "Send /cancel to stop talking to me.\n\n",
         reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="select model"
+            reply_keyboard,
+            resize_keyboard=True,
+            is_persistent=True,
+            one_time_keyboard=True,
+            input_field_placeholder="select model"
         ),
     )
-    return CHAT
-
-
-def time_to_str(t) -> str:
-    return time.strftime('%Y-%m-%d %H:%M:%S', t)
-
-
-def time_str_to_int(s):
-    datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
-
-
-def str_to_time(s):
-    return datetime.datetime.strftime(s, '%Y-%m-%d %H:%M:%S')
+    return CREATE_PROMPT
 
 
 async def select_history(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -279,7 +308,11 @@ async def select_history(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     await update.message.reply_text(
         "Hi, you have came back to " + session + " session. Continue to have fun !",
         reply_markup=ReplyKeyboardMarkup(
-            [['/cancel']], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="chat"
+            [['/cancel']],
+            resize_keyboard=True,
+            is_persistent=True,
+            one_time_keyboard=True,
+            input_field_placeholder="chat"
         ),
     )
     return SEND_PROMPT_TEXT
@@ -298,7 +331,11 @@ async def create_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
         "Hello, I'm " + cursor[user.name]['model'] + ", "
                                                      "you can ask me a question or chat to me, or send /cancel if you don't want to.",
         reply_markup=ReplyKeyboardMarkup(
-            [['/cancel']], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="select model"
+            [['/cancel']],
+            resize_keyboard=True,
+            is_persistent=True,
+            one_time_keyboard=True,
+            input_field_placeholder="select model"
         ),
     )
     return SEND_PROMPT_TEXT
@@ -346,7 +383,11 @@ async def send_prompt_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -
             await update.message.reply_text(
                 response_data[x:x + max_len],
                 reply_markup=ReplyKeyboardMarkup(
-                    [['/cancel']], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="chat"
+                    [['/cancel']],
+                    resize_keyboard=True,
+                    is_persistent=True,
+                    one_time_keyboard=True,
+                    input_field_placeholder="chat"
                 ),
             )
     else:
@@ -354,7 +395,11 @@ async def send_prompt_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -
             escape(response),
             parse_mode="MarkdownV2",
             reply_markup=ReplyKeyboardMarkup(
-                [['/cancel']], resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="chat"
+                [['/cancel']],
+                resize_keyboard=True,
+                is_persistent=True,
+                one_time_keyboard=True,
+                input_field_placeholder="chat"
             ),
         )
     return SEND_PROMPT_TEXT
@@ -375,10 +420,10 @@ def invoke_model(factory: str, model: str, messages) -> str:
     return escape(response)
 
 
+# main entrypoint
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your token.
-    # 7654069819:AAFXrxCc2ym-JdLm_1zgBconCEQ1bgQKnCo
     token = os.environ.get('BOT_TOKEN')
     if token is None or token == "":
         print('BOT_TOKEN not found !')
@@ -391,25 +436,31 @@ def main() -> None:
         states={},
         fallbacks=[],
     )
-
     # gpt handler
     gpt_handler = ConversationHandler(
-        entry_points=[CommandHandler("gpt", gpt_start)],  # 开始
+        entry_points=[CommandHandler("gpt", chatgpt_start)],  # 开始
         states={
             CHECK_HISTORY: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, check_history)],  # 检查聊天历史
             CONTINUE_LAST: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, create_prompt)],  # 继续上一次聊天
             SELECT_HISTORY: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, select_history)],  # 选择历史聊天
             NEW_CHAT: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, new_chat)],  # 新建聊天
-            CHAT: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, create_prompt)],  # 开始聊天
-            SEND_PROMPT_TEXT: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, send_prompt_text)]  # 发送普通文本 prompt
+            # 创建提示词
+            CREATE_PROMPT: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, create_prompt)],
+            # 发送普通文本提示词
+            SEND_PROMPT_TEXT: [CommandHandler("cancel", cancel), MessageHandler(filters.TEXT, send_prompt_text)],
         },
         fallbacks=[],
     )
-
     # deepseek handler
     deepseek_handler = ConversationHandler(
         entry_points=[CommandHandler("deepseek", readme)],
         states={
+            CHECK_HISTORY: [],
+            CONTINUE_LAST: [],
+            SELECT_HISTORY: [],
+            NEW_CHAT: [],
+            CREATE_PROMPT: [],
+            SEND_PROMPT_TEXT: [],
         },
         fallbacks=[],
     )
